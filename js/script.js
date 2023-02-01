9a515eeb9c7c39093529957dafd54846b264de06
TysonOS <tysonoliveira2013@gmail.com>
2023-02-01 19:32:13 -0300

    add validation for empty message textarea

--- js/script.js
+++ js/script.js
@@ -3,33 +3,43 @@ let result = document.querySelector("#result_textarea");
 let encryptButton = document.querySelector("#encrypt_button");
 let decryptButton = document.querySelector("#decrypt_button");
 let copyButton = document.querySelector("#copy_button");
-let clearButton = document.querySelector("#clear_button")
+let clearButton = document.querySelector("#clear_button");
 
 function encrypt(){
-    let text = message.value;
-    let encryptedText = text.replaceAll("e", "enter")
-    .replaceAll("i", "imes")
-    .replaceAll("a", "ai")
-    .replaceAll("o", "ober")
-    .replaceAll("u", "ufat");
-    result.value = encryptedText;
-    showResult();
+    let validMessage = message.value.trim();
+    if(validMessage){
+        let text = message.value;
+        let encryptedText = text.replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("o", "ober")
+        .replaceAll("u", "ufat");
+        result.value = encryptedText;
+        showResult();
+    }else{
+        alert("Digite uma mensagem válida!");
+    }
 }
 
 function decrypt(){
-    let text = message.value;
-    let decryptedText = text.replaceAll("enter", "e")
-    .replaceAll("imes", "i")
-    .replaceAll("ai", "a")
-    .replaceAll("ober", "o")
-    .replaceAll("ufat", "u");
-    result.value = decryptedText;
-    showResult();
+    let validMessage = message.value.trim();
+    if (validMessage){
+        let text = message.value;
+        let decryptedText = text.replaceAll("enter", "e")
+        .replaceAll("imes", "i")
+        .replaceAll("ai", "a")
+        .replaceAll("ober", "o")
+        .replaceAll("ufat", "u");
+        result.value = decryptedText;
+        showResult();
+    }else{
+        alert("Digite uma mensagem!");
+    }
+    
 }
 
 function copy(){
     if(result.value){
-        // window.alert("copied")
         window.navigator.clipboard.writeText(result.value);
     }
     
@@ -40,16 +50,17 @@ function clear(){
     result.value = "";
     message.focus();
 
+    showResult();
 }
 
 function showResult(){
     let noMessageDiv = document.querySelector("#no_message");
     let resultDiv = document.querySelector("#result");
-    
-    if(!noMessageDiv.classList.contains("hidden")){
-        noMessageDiv.classList.toggle("hidden");
-        resultDiv.classList.toggle("hidden");
-    }
+    let resultButtons = document.querySelector("#result .buttons");
+
+    noMessageDiv.classList.toggle("hidden");
+    resultDiv.classList.toggle("hidden");
+    resultButtons.classList.toggle("hidden");
 }
 
 encryptButton.onclick = encrypt;
